72eafd999d6dab6cd9a88dc9a8042fbd75797106
jonny-novacom <jonny.edwards@novacom.group>
2022-10-28 14:28:09 +0100

    removed tailwind

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from './src/components/ContextProviders/ThemeProvider';
 import { GlobalStyle } from './src/components/Layout/sharedStyles/globalStyles';
-import './src/styles/global.css';
+// import './src/styles/global.css';
 
 export const wrapPageElement = ({ element }) => (
   <>
